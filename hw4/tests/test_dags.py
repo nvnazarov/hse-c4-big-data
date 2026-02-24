0398e0c366b951a525e21d2b84b2aee8e7d0b651
Nikita Nazarov <nvnazarov.me@gmail.com>
2026-02-24 19:48:39 +0300

    feat: add benchmark test

--- hw4/tests/test_dags.py
+++ hw4/tests/test_dags.py
@@ -1,2 +1,10 @@
-def test_dag_loading():
-    import dags.download_and_clean_products  # type: ignore # noqa: F401
+import pytest
+
+from typing import Any
+
+
+@pytest.mark.benchmark
+def test_dag_loading(benchmark: Any):
+    @benchmark
+    def import_dag():  # type: ignore
+        import dags.download_and_clean_products  # type: ignore # noqa: F401
