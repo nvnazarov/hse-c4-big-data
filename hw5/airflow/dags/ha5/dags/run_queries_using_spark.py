74d6db9fb82a50f1076e54765f9e46a09cbdc1ef
Nikita Nazarov <nvnazarov.me@gmail.com>
2026-03-10 19:02:49 +0300

    fix+feat: add remaining queries + fix existing queries

--- hw5/airflow/dags/ha5/dags/run_queries_using_spark.py
+++ hw5/airflow/dags/ha5/dags/run_queries_using_spark.py
@@ -12,20 +12,27 @@ def run_queries_using_spark():
 
     @task
     def q1():
+        """
+        Для каждого клиента вывести ФИО и общую сумму его кредитов,
+        а также сумму по всем кредитам.
+        """
+
         SparkSqlHook(
             sql=(
                 f"USE {ICEBERG_SCHEMA};"
                 "INSERT OVERWRITE DIRECTORY 's3a://bank/results/q1' "
                 "USING csv "
                 "SELECT "
-                "   clients.id, "
-                "   clients.full_name, "
-                "   SUM(credits.amount) AS total_credits "
+                "   clients.id AS client_id, "
+                "   clients.full_name AS client_full_name, "
+                "   SUM(credits.amount) AS total_client_credits, "
+                "   SUM(SUM(credits.amount)) OVER () AS total_credits "
                 "FROM "
                 "   clients "
                 "JOIN "
                 "   credits ON credits.client_id = clients.id "
-                "GROUP BY clients.id, clients.full_name;"
+                "GROUP BY "
+                "   clients.id, clients.full_name;"
             ),
             conn_id=SPARK_CONNECTION_ID,
             master=SPARK_MASTER,
@@ -35,6 +42,11 @@ def run_queries_using_spark():
 
     @task
     def q2():
+        """
+        Для каждого клиента вывести ФИО, id вклада и сумму вклада,
+        а также максимальную сумму вклада клиента.
+        """
+
         SparkSqlHook(
             sql=(
                 f"USE {ICEBERG_SCHEMA};"
@@ -42,10 +54,10 @@ def run_queries_using_spark():
                 "USING csv "
                 "SELECT "
                 "   clients.id AS client_id, "
-                "   clients.full_name, "
+                "   clients.full_name AS client_full_name, "
                 "   deposits.id AS deposit_id, "
-                "   deposits.amount, "
-                "   MAX(deposits.amount) OVER (PARTITION BY clients.id) AS max_amount "
+                "   deposits.amount AS deposit_amount, "
+                "   MAX(deposits.amount) OVER (PARTITION BY clients.id) AS max_client_deposit_amount "
                 "FROM "
                 "   deposits "
                 "JOIN "
@@ -59,15 +71,21 @@ def run_queries_using_spark():
 
     @task
     def q3():
+        """
+        Для каждой транзакции вывести id транзакции, ФИО отправителя
+        и сумму перевода, а также среднюю сумму перевода по всем
+        транзакциям отправителя.
+        """
+
         SparkSqlHook(
             sql=(
                 f"USE {ICEBERG_SCHEMA};"
                 "INSERT OVERWRITE DIRECTORY 's3a://bank/results/q3' "
                 "USING csv "
                 "SELECT "
-                "   transactions.id, "
-                "   clients.full_name, "
-                "   transactions.amount, "
+                "   transactions.id AS transaction_id, "
+                "   clients.full_name AS client_full_name, "
+                "   transactions.amount AS transaction_amount, "
                 "   AVG(transactions.amount) OVER (PARTITION BY clients.id) AS avg_amount "
                 "FROM "
                 "   transactions "
@@ -84,23 +102,27 @@ def run_queries_using_spark():
 
     @task
     def q4():
+        """
+        Для каждого актива вывести id актива, ФИО владельца и количество,
+        а также суммарное количество активов клиента.
+        """
+
         SparkSqlHook(
             sql=(
                 f"USE {ICEBERG_SCHEMA};"
                 "INSERT OVERWRITE DIRECTORY 's3a://bank/results/q4' "
                 "USING csv "
                 "SELECT "
-                "   actives.id as active_id, "
-                "   clients.full_name as owner_full_name, "
-                "   actives.count, "
-                "   SUM(actives.count) AS total_actives "
+                "   actives.id AS active_id, "
+                "   clients.full_name AS client_full_name, "
+                "   actives.count AS active_count, "
+                "   SUM(actives.count) OVER (PARTITION BY clients.id) AS total_client_actives "
                 "FROM "
                 "   actives "
                 "JOIN "
                 "   bank_accounts ON bank_accounts.id = actives.bank_account_id "
                 "JOIN "
-                "   clients ON clients.id = bank_accounts.client_id "
-                "GROUP BY actives.id, clients.full_name, actives.count;"
+                "   clients ON clients.id = bank_accounts.client_id;"
             ),
             conn_id=SPARK_CONNECTION_ID,
             master=SPARK_MASTER,
@@ -110,16 +132,21 @@ def run_queries_using_spark():
 
     @task
     def q5():
+        """
+        Для каждого клиента вывести ФИО и зарплату, а также ранг
+        зарплаты в пределах его отрасли.
+        """
+
         SparkSqlHook(
             sql=(
                 f"USE {ICEBERG_SCHEMA};"
                 "INSERT OVERWRITE DIRECTORY 's3a://bank/results/q5' "
                 "USING csv "
                 "SELECT "
-                "   clients.full_name, "
-                "   profiles.salary, "
-                "   profiles.work_area, "
-                "   DENSE_RANK() OVER (PARTITION BY profiles.work_area ORDER BY profiles.salary DESC) AS salary_rank "
+                "   clients.full_name AS client_full_name, "
+                "   profiles.salary AS client_salary, "
+                "   profiles.work_area AS client_work_area, "
+                "   DENSE_RANK() OVER (PARTITION BY profiles.work_area ORDER BY profiles.salary DESC) AS client_salary_rank "
                 "FROM "
                 "   profiles "
                 "JOIN "
@@ -133,6 +160,11 @@ def run_queries_using_spark():
 
     @task
     def q6():
+        """
+        Для каждого кредита вывести id кредита, ФИО клиента и сумму, а
+        также порядковый номер кредита клиента.
+        """
+
         SparkSqlHook(
             sql=(
                 f"USE {ICEBERG_SCHEMA};"
@@ -140,9 +172,9 @@ def run_queries_using_spark():
                 "USING csv "
                 "SELECT "
                 "   credits.id AS credit_id, "
-                "   clients.full_name, "
-                "   credits.amount, "
-                "   ROW_NUMBER() OVER (PARTITION BY credits.client_id ORDER BY credits.dt_opened, credits.id) AS counter "
+                "   clients.full_name AS client_full_name, "
+                "   credits.amount AS credit_amount, "
+                "   ROW_NUMBER() OVER (PARTITION BY credits.client_id ORDER BY credits.dt_opened, credits.id) AS index "
                 "FROM "
                 "   credits "
                 "JOIN "
@@ -156,6 +188,11 @@ def run_queries_using_spark():
 
     @task
     def q7():
+        """
+        Для каждого вклада вывести id вклада, ФИО клиента и сумму, а
+        также среднюю сумму вкладов по всем клиентам.
+        """
+
         SparkSqlHook(
             sql=(
                 f"USE {ICEBERG_SCHEMA};"
@@ -163,9 +200,9 @@ def run_queries_using_spark():
                 "USING csv "
                 "SELECT "
                 "   deposits.id AS deposit_id, "
-                "   clients.full_name, "
-                "   deposits.amount, "
-                "   (SELECT AVG(deposits.amount) FROM deposits) AS avg_amount "
+                "   clients.full_name AS client_full_name, "
+                "   deposits.amount AS deposit_amount, "
+                "   (SELECT AVG(deposits.amount) FROM deposits) AS avg_deposit_amount "
                 "FROM "
                 "   deposits "
                 "JOIN "
@@ -179,6 +216,12 @@ def run_queries_using_spark():
 
     @task
     def q8():
+        """
+        Для каждой транзакции вывести id транзакции, тип счёта
+        отправителя и сумму, а также кумулятивную сумму переводов
+        где данный счёт был отправителем.
+        """
+
         SparkSqlHook(
             sql=(
                 f"USE {ICEBERG_SCHEMA};"
@@ -187,16 +230,12 @@ def run_queries_using_spark():
                 "SELECT "
                 "   transactions.id AS transaction_id, "
                 "   bank_accounts.type AS bank_account_type, "
-                "   transactions.amount, "
-                "   SUM(transactions.amount) as cum_amount "
+                "   transactions.amount AS transaction_amount, "
+                "   SUM(transactions.amount) OVER (PARTITION BY transactions.send_bank_account_id) as cum_amount "
                 "FROM "
                 "   transactions "
                 "JOIN "
-                "   bank_accounts ON bank_accounts.id = transactions.send_bank_account_id "
-                "GROUP BY "
-                "   transactions.id, "
-                "   bank_accounts.type, "
-                "   transactions.amount;"
+                "   bank_accounts ON bank_accounts.id = transactions.send_bank_account_id;"
             ),
             conn_id=SPARK_CONNECTION_ID,
             master=SPARK_MASTER,
@@ -206,6 +245,11 @@ def run_queries_using_spark():
 
     @task
     def q9():
+        """
+        Для каждого клиента вывести ФИО и процент по кредиту, а также
+        минимальный процент среди всех кредитов клиента.
+        """
+
         SparkSqlHook(
             sql=(
                 f"USE {ICEBERG_SCHEMA};"
@@ -213,15 +257,13 @@ def run_queries_using_spark():
                 "USING csv "
                 "SELECT "
                 "   clients.id AS client_id, "
-                "   clients.full_name, "
-                "   credits.percent, "
-                "   MIN(CAST(credits.percent AS NUMERIC(3,2))) AS min_percent "
+                "   clients.full_name AS client_full_name, "
+                "   credits.percent AS credit_percent, "
+                "   MIN(CAST(credits.percent AS NUMERIC(3,2))) OVER (PARTITION BY clients.id) AS min_client_percent "
                 "FROM "
                 "   credits "
                 "JOIN "
-                "   clients ON clients.id = credits.client_id "
-                "GROUP BY "
-                "   clients.id, clients.full_name, credits.id, credits.percent;"
+                "   clients ON clients.id = credits.client_id;"
             ),
             conn_id=SPARK_CONNECTION_ID,
             master=SPARK_MASTER,
@@ -231,11 +273,22 @@ def run_queries_using_spark():
 
     @task
     def q10():
+        """
+        Для каждой выплаты по кредиту вывести id выплаты и сумму выплаты,
+        а также максимальную выплату по кредиту.
+        """
+
         SparkSqlHook(
             sql=(
                 f"USE {ICEBERG_SCHEMA};"
                 "INSERT OVERWRITE DIRECTORY 's3a://bank/results/q10' "
                 "USING csv "
+                "SELECT "
+                "   credit_payments.id AS payment_id, "
+                "   credit_payments.amount AS payment_amount, "
+                "   MAX(credit_payments.amount) OVER (PARTITION BY credit_payments.credit_id) AS max_credit_payment "
+                "FROM "
+                "   credit_payments;"
             ),
             conn_id=SPARK_CONNECTION_ID,
             master=SPARK_MASTER,
